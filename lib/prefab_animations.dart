45fcf2a530e61b73ea841608f84601647bc8a6fb
Alexis <texs.mv@gmail.com>
2020-08-11 09:19:48 -0500

    event animation version 0.5

--- lib/prefab_animations.dart
+++ lib/prefab_animations.dart
@@ -1,5 +1,8 @@
 library prefab_animations;
 
+import 'package:prefab_animations/event_animation/event_animation.dart';
+
+
 // /// A Calculator.
 // class Calculator {
 //   /// Returns [value] plus 1.
