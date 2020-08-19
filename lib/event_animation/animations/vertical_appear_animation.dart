d502a20afe42681577d1017ee20764566cbd8ba4
Alexis Mendoza <texs.mv@gmail.com>
2020-08-18 23:53:14 -0500

    Tween arguments added to vertial appear animation

--- lib/event_animation/animations/vertical_appear_animation.dart
+++ lib/event_animation/animations/vertical_appear_animation.dart
@@ -6,6 +6,7 @@ class VerticalAppearAnimation extends AnimatedWidget {
   double minScale;
   CurveTween scaleTween;
   CurveTween opacityTween;
+  CurveTween translateTween;
   VerticalAppearAnimation({
     Key key,
     @required AnimationController controller,
@@ -13,6 +14,7 @@ class VerticalAppearAnimation extends AnimatedWidget {
     this.minScale = 0.8,
     this.scaleTween,
     this.opacityTween,
+    this.translateTween,
     this.child,
   }) : super(key: key, listenable: controller);
 
@@ -22,10 +24,12 @@ class VerticalAppearAnimation extends AnimatedWidget {
   Widget build(BuildContext context) {
     if (scaleTween == null) scaleTween = CurveTween(curve: Curves.linear);
     if (opacityTween == null) opacityTween = CurveTween(curve: Curves.linear);
+    if (translateTween == null)
+      translateTween = CurveTween(curve: Curves.linear);
     return Transform.scale(
       scale: 1 - ((1 - scaleTween.evaluate(_progress)) * (1 - minScale)),
       child: Opacity(
-        opacity: _progress.value,
+        opacity: opacityTween.evaluate(_progress),
         child: Transform.translate(
           offset: Offset(
               0,
