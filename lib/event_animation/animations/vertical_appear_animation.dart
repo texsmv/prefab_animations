e77b49634ad23849d0c36013111bfd0632e461b1
Alexis Mendoza <texs.mv@gmail.com>
2020-08-18 23:46:54 -0500

    Tween arguments added to vertial appear animation

--- lib/event_animation/animations/vertical_appear_animation.dart
+++ lib/event_animation/animations/vertical_appear_animation.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
-
 class VerticalAppearAnimation extends AnimatedWidget {
   Widget child;
   double verticalOffSet;
   double minScale;
+  CurveTween scaleTween;
+  CurveTween opacityTween;
   VerticalAppearAnimation({
-                          Key key, 
-                          @required AnimationController controller, 
-                          this.verticalOffSet = 25,
-                          this.minScale = 0.8,
-                          this.child, 
-                        })
-      : super(key: key, listenable: controller);
+    Key key,
+    @required AnimationController controller,
+    this.verticalOffSet = 25,
+    this.minScale = 0.8,
+    this.scaleTween,
+    this.opacityTween,
+    this.child,
+  }) : super(key: key, listenable: controller);
 
   Animation<double> get _progress => listenable;
 
   @override
   Widget build(BuildContext context) {
+    if (scaleTween == null) scaleTween = CurveTween(curve: Curves.linear);
+    if (opacityTween == null) opacityTween = CurveTween(curve: Curves.linear);
     return Transform.scale(
-      scale: 1 - ((1 - _progress.value) * (1 - minScale)) ,
+      scale: 1 - ((1 - scaleTween.evaluate(_progress)) * (1 - minScale)),
       child: Opacity(
         opacity: _progress.value,
         child: Transform.translate(
-          offset: Offset(0, _progress.value * verticalOffSet - verticalOffSet),
+          offset: Offset(
+              0,
+              opacityTween.evaluate(_progress) * verticalOffSet -
+                  verticalOffSet),
           child: child,
         ),
       ),
