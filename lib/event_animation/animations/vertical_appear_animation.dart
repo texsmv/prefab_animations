05f2c11c52be970a84360060d10f6cd8f31f3485
Alexis Mendoza <texs.mv@gmail.com>
2020-09-01 14:40:18 -0500

    [Issue #102] personalized tweens for verticalAppear animation

--- lib/event_animation/animations/vertical_appear_animation.dart
+++ lib/event_animation/animations/vertical_appear_animation.dart
@@ -4,11 +4,17 @@ class VerticalAppearAnimation extends AnimatedWidget {
   Widget child;
   double verticalOffSet;
   double minScale;
+  CurveTween scaleTween;
+  CurveTween opacityTween;
+  CurveTween translateTween;
   VerticalAppearAnimation({
     Key key,
     @required AnimationController controller,
     this.verticalOffSet = 25,
     this.minScale = 0.8,
+    this.scaleTween,
+    this.opacityTween,
+    this.translateTween,
     this.child,
   }) : super(key: key, listenable: controller);
 
@@ -16,12 +22,19 @@ class VerticalAppearAnimation extends AnimatedWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (scaleTween == null) scaleTween = CurveTween(curve: Curves.linear);
+    if (opacityTween == null) opacityTween = CurveTween(curve: Curves.linear);
+    if (translateTween == null)
+      translateTween = CurveTween(curve: Curves.linear);
     return Transform.scale(
-      scale: 1 - ((1 - _progress.value) * (1 - minScale)),
+      scale: 1 - ((1 - scaleTween.evaluate(_progress)) * (1 - minScale)),
       child: FadeTransition(
-        opacity: _progress,
+        opacity: opacityTween.animate(_progress),
         child: Transform.translate(
-          offset: Offset(0, _progress.value * verticalOffSet - verticalOffSet),
+          offset: Offset(
+              0,
+              translateTween.evaluate(_progress) * verticalOffSet -
+                  verticalOffSet),
           child: child,
         ),
       ),
