092f019e42b1e0f8ecdb3f0010bb6b5fd8e56bab
Alexis Mendoza <texs.mv@gmail.com>
2020-09-23 21:42:15 -0500

    added disappear animation

---
+++ lib/event_animation/animations/vertical_disappear_animation.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class VerticalDisappearAnimation extends AnimatedWidget {
+  Widget child;
+  double verticalOffSet;
+  double minScale;
+  CurveTween scaleTween;
+  CurveTween opacityTween;
+  CurveTween translateTween;
+  VerticalDisappearAnimation({
+    Key key,
+    @required AnimationController controller,
+    this.verticalOffSet = 25,
+    this.minScale = 0.8,
+    this.scaleTween,
+    this.opacityTween,
+    this.translateTween,
+    this.child,
+  }) : super(key: key, listenable: controller);
+
+  Animation<double> get _progress => listenable;
+
+  @override
+  Widget build(BuildContext context) {
+    if (scaleTween == null) scaleTween = CurveTween(curve: Curves.linear);
+    if (opacityTween == null)
+      opacityTween = CurveTween(curve: Curves.linear.flipped);
+    if (translateTween == null)
+      translateTween = CurveTween(curve: Curves.linear);
+    return Transform.scale(
+      scale: 1 - ((1 - scaleTween.evaluate(_progress)) * (1 - minScale)),
+      child: FadeTransition(
+        opacity: ReverseAnimation(opacityTween.animate(_progress)),
+        child: Transform.translate(
+          offset: Offset(
+              0,
+              translateTween.evaluate(_progress) * verticalOffSet -
+                  verticalOffSet),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
